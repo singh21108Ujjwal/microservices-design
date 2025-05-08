ac0f17cafd6e9544275ba78c9ad3d9dfa1ee0989
Ujjwal Kumar <ujjawalsingh21108@gmail.com>
2025-05-08 19:47:06 +0530

    Global filters included

--- APIGateway/src/main/java/com/appsdeveloperblog/APIGateway/GlobalFiltersConfiguration.java
+++ APIGateway/src/main/java/com/appsdeveloperblog/APIGateway/GlobalFiltersConfiguration.java
@@ -1,6 +1,15 @@
 package com.appsdeveloperblog.APIGateway;
 
 
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class GlobalFiltersConfiguration {
+
+    public GlobalFilter secondFilter(){
+        return ((exchange, chain) -> {
+            return chain.filter(exchange);
+        });
+    }
 }
